e7e55622c2470c4cb24ff8cee6174d2107a2f666
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2024-01-10 01:48:24 +0530

    Adding cpp file

---
+++ babbarpatter.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     for (int j =0; j<4; j++)
+    //     {
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cout << "* ";
+        }
+        cout<<endl;
+        
+    }
+    
+    
+    return 0;
+}
