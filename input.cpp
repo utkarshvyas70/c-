886328771af2bdb68c209951e740591932e7f82e
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2024-01-09 21:01:30 +0530

    Adding cpp file

---
+++ input.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int x;
+    // cout<< "enter the number: ";
+    // cin>>x;
+    // cout<<"Your no. is: "<<x;
+
+    // calc
+    int x,y;
+    int sum;
+    cout<<"Enter the first digit: ";
+    cin>>x;
+    cout<<"Enter the second digit: ";
+    cin>>y;
+    sum=x+y;
+    cout<<"Your desired addition is sum: "<<sum;
+    return 0;
+}
