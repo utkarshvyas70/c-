3d1478acc4a1804b0c7d35a8eb47a8d8f428a1f4
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2024-01-09 21:01:07 +0530

    adding cpp file

---
+++ newLine.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello world! \n";
+    cout<<"dsfs";
+    return 0;
+}
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello world!"<<endl;
+    cout<<"dsfs";
+    return 0;
+}
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello world \"Johnny\".";
+    return 0;
+}
