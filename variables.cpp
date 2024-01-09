f52e390238c974a32f904e4bb829b6ee43c9e5a6
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2024-01-09 21:01:55 +0530

    adding cpp file

---
+++ variables.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int myNum=15;
+    // int myNum;
+    // myNum=15;
+    int myNum=15;
+    myNum=10;
+    double myfloatNum=5.44;
+    cout<<myfloatNum<<endl;
+    char myLetter='D';
+    cout<<myLetter<<endl;
+    string myText="Hello world";
+    cout<<myText<<endl;
+    bool myboolean=true;
+    cout<<myboolean<<endl;
+    cout<<myNum<<endl;
+    return 0;
+}
