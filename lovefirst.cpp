beb6ef4e91f5ff7dfa2bdd20ce0ffc9b2a906214
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2024-01-09 22:09:02 +0530

    Adding cpp file

---
+++ lovefirst.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Love Babar"<<endl;
+    cout<<"Love Babar";
+    int num=50;
+    cout<<num<<endl;
+    cout<<sizeof(num)<<endl;
+
+    long number=32;
+    cout<<number<<endl;
+    cout<<sizeof(number)<<endl;
+    // here now it will come 8 but actually it is 4
+    int num;
+    cout<<num<<endl;
+    // garbage value/ random 
+    return 0;
+}
